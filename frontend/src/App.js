92ec0ed4799771f6de011b2055e2f4a623121236
Marlon (Mj) Burog <spicyrice09@gmail.com>
2024-08-03 10:05:21 -0700

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Main from './main';
+import Dash from './routes/dashboard';
+import Privacy from './routes/privacy';
 function App() {
   return (
     <Router>
       <Routes>
         <Route exact path="*" element={<Main />} />
-        <Route exact path="/signin" element={<Signin />} />
+        <Route exact path="/privacy" element={<Privacy />} />
+        <Route exact path="/dashboard" element={<Dash />} />
       </Routes>
     </Router>
   );
---
+++ frontend/src/routes/dashboard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Dash(){
+    return(
+        <div className='main'>
+            <div className='prompt' />
+            <div className='left-dashboard'>
+            
+            </div> 
+            <div className='map'>
+
+            </div>
+        </div>
+    )
+}
