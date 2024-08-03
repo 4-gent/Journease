6c5a4fd6a56d98c2e1e2fc84cbef19d3c91418cc
Sandeep <k.sandeep.shandilya39@gmail.com>
2024-08-03 16:05:34 -0700

    basic dashboard done

--- frontend/src/routes/privacy.js
+++ frontend/src/routes/privacy.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import '../public/privacy.css';
+import Dashboard from './dashboard.js';
 
 export default function Privacy() {
     const [privacySettings, setPrivacySettings] = useState({
@@ -17,98 +18,97 @@ export default function Privacy() {
             ...prevSettings,
             [name]: checked
         }));
+        localStorage.setItem('privacySettings', JSON.stringify(privacySettings));
     };
 
     const handleSubmit = (e) => {
         e.preventDefault();
         console.log('Selected Privacy Options:', privacySettings);
         setShowUserInfo(true); // Show user info after submission
+        localStorage.setItem('privacySettings', JSON.stringify(privacySettings));
     };
 
     const userInfo = JSON.parse(localStorage.getItem('userInfo'));
     const accessToken = localStorage.getItem('accessToken');
 
     return (
-        <div className="privacy-container">
-            <h1>Privacy Settings</h1>
-            <form onSubmit={handleSubmit} className="privacy-form">
-                <div className="switch">
-                    <input
-                        type="checkbox"
-                        id="cookies"
-                        name="cookies"
-                        checked={privacySettings.cookies}
-                        onChange={handleToggleChange}
-                    />
-                    <label htmlFor="cookies">
-                        <span className="slider"></span>
-                        Allow Cookies
-                    </label>
-                </div>
-                <div className="switch">
-                    <input
-                        type="checkbox"
-                        id="email"
-                        name="email"
-                        checked={privacySettings.email}
-                        onChange={handleToggleChange}
-                    />
-                    <label htmlFor="email">
-                        <span className="slider"></span>
-                        Allow Email Access
-                    </label>
-                </div>
-                <div className="switch">
-                    <input
-                        type="checkbox"
-                        id="history"
-                        name="history"
-                        checked={privacySettings.history}
-                        onChange={handleToggleChange}
-                    />
-                    <label htmlFor="history">
-                        <span className="slider"></span>
-                        Allow History Access
-                    </label>
-                </div>
-                <div className="switch">
-                    <input
-                        type="checkbox"
-                        id="calendar"
-                        name="calendar"
-                        checked={privacySettings.calendar}
-                        onChange={handleToggleChange}
-                    />
-                    <label htmlFor="calendar">
-                        <span className="slider"></span>
-                        Allow Calendar Access
-                    </label>
-                </div>
-                <div className="switch">
-                    <input
-                        type="checkbox"
-                        id="documents"
-                        name="documents"
-                        checked={privacySettings.documents}
-                        onChange={handleToggleChange}
-                    />
-                    <label htmlFor="documents">
-                        <span className="slider"></span>
-                        Allow Documents Access
-                    </label>
-                </div>
-                <button type="submit" className="submit-button">Submit Options</button>
-            </form>
-
-            {showUserInfo && userInfo && (
-                <div className="user-info">
-                    <h2>User Information</h2>
-                    <p><strong>Name:</strong> {userInfo.name}</p>
-                    <p><strong>Email:</strong> {userInfo.email}</p>
-                    <img src={userInfo.picture} alt="Profile" width="100" />
-                    <p><strong>Access Token:</strong> {accessToken}</p>
-                </div>
+        <div>
+            {showUserInfo && userInfo ? (
+                <Dashboard />
+            ):(
+                <div className="privacy-container">
+                    <h1>Privacy Settings</h1>
+                    <form onSubmit={handleSubmit} className="privacy-form">
+                        <div className="switch">
+                            <input
+                                type="checkbox"
+                                id="cookies"
+                                name="cookies"
+                                checked={privacySettings.cookies}
+                                onChange={handleToggleChange}
+                            />
+                            <label htmlFor="cookies">
+                                <span className="slider"></span>
+                                Allow Cookies
+                            </label>
+                        </div>
+                        <div className="switch">
+                            <input
+                                type="checkbox"
+                                id="email"
+                                name="email"
+                                checked={privacySettings.email}
+                                onChange={handleToggleChange}
+                            />
+                            <label htmlFor="email">
+                                <span className="slider"></span>
+                                Allow Email Access
+                            </label>
+                        </div>
+                        <div className="switch">
+                            <input
+                                type="checkbox"
+                                id="history"
+                                name="history"
+                                checked={privacySettings.history}
+                                onChange={handleToggleChange}
+                            />
+                            <label htmlFor="history">
+                                <span className="slider"></span>
+                                Allow History Access
+                            </label>
+                        </div>
+                        <div className="switch">
+                            <input
+                                type="checkbox"
+                                id="calendar"
+                                name="calendar"
+                                checked={privacySettings.calendar}
+                                onChange={handleToggleChange}
+                            />
+                            <label htmlFor="calendar">
+                                <span className="slider"></span>
+                                Allow Calendar Access
+                            </label>
+                        </div>
+                        <div className="switch">
+                            <input
+                                type="checkbox"
+                                id="documents"
+                                name="documents"
+                                checked={privacySettings.documents}
+                                onChange={handleToggleChange}
+                            />
+                            <label htmlFor="documents">
+                                <span className="slider"></span>
+                                Allow Documents Access
+                            </label>
+                        </div>
+                        <button type="submit" className="submit-button">Submit Options</button>
+                    </form>  
+                </div>  
             )}
         </div>
+        
     );
 }
