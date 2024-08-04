cde4b2ea0b2730895fab90ae735fc03e17d09e74
Sandeep <k.sandeep.shandilya39@gmail.com>
2024-08-03 19:53:06 -0700

    Privacy needs to be fixed

--- frontend/src/routes/privacy.js
+++ frontend/src/routes/privacy.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import '../public/privacy.css';
+import styles from '../public/Privacy.module.css'; // Import the CSS module
 import Dashboard from './dashboard.js';
 
 export default function Privacy() {
@@ -32,84 +32,87 @@ export default function Privacy() {
     const accessToken = localStorage.getItem('accessToken');
 
     return (
-        <div>
+        <div className={styles.privacyPageWrapper}>
             {showUserInfo && userInfo ? (
                 <Dashboard />
-            ):(
-                <div className="privacy-container">
+            ) : (
+                <div className={styles.privacyContainer}>
                     <h1>Privacy Settings</h1>
-                    <form onSubmit={handleSubmit} className="privacy-form">
-                        <div className="switch">
-                            <input
-                                type="checkbox"
-                                id="cookies"
-                                name="cookies"
-                                checked={privacySettings.cookies}
-                                onChange={handleToggleChange}
-                            />
-                            <label htmlFor="cookies">
-                                <span className="slider"></span>
-                                Allow Cookies
-                            </label>
-                        </div>
-                        <div className="switch">
-                            <input
-                                type="checkbox"
-                                id="email"
-                                name="email"
-                                checked={privacySettings.email}
-                                onChange={handleToggleChange}
-                            />
-                            <label htmlFor="email">
-                                <span className="slider"></span>
-                                Allow Email Access
-                            </label>
-                        </div>
-                        <div className="switch">
-                            <input
-                                type="checkbox"
-                                id="history"
-                                name="history"
-                                checked={privacySettings.history}
-                                onChange={handleToggleChange}
-                            />
-                            <label htmlFor="history">
-                                <span className="slider"></span>
-                                Allow History Access
-                            </label>
-                        </div>
-                        <div className="switch">
-                            <input
-                                type="checkbox"
-                                id="calendar"
-                                name="calendar"
-                                checked={privacySettings.calendar}
-                                onChange={handleToggleChange}
-                            />
-                            <label htmlFor="calendar">
-                                <span className="slider"></span>
-                                Allow Calendar Access
-                            </label>
-                        </div>
-                        <div className="switch">
-                            <input
-                                type="checkbox"
-                                id="documents"
-                                name="documents"
-                                checked={privacySettings.documents}
-                                onChange={handleToggleChange}
-                            />
-                            <label htmlFor="documents">
-                                <span className="slider"></span>
-                                Allow Documents Access
-                            </label>
-                        </div>
-                        <button type="submit" className="submit-button">Submit Options</button>
-                    </form>  
-                </div>  
+                    <div className={styles.form}>
+                        <form onSubmit={handleSubmit} className={styles.privacyForm}>
+                            <div className={styles.switch}>
+                                <input
+                                    type="checkbox"
+                                    id="cookies"
+                                    name="cookies"
+                                    checked={privacySettings.cookies}
+                                    onChange={handleToggleChange}
+                                />
+                                <label htmlFor="cookies">
+                                    <span className={styles.slider}></span>
+                                    Allow Cookies
+                                </label>
+                            </div>
+                            <div className={styles.switch}>
+                                <input
+                                    type="checkbox"
+                                    id="email"
+                                    name="email"
+                                    checked={privacySettings.email}
+                                    onChange={handleToggleChange}
+                                />
+                                <label htmlFor="email">
+                                    <span className={styles.slider}></span>
+                                    Allow Email Access
+                                </label>
+                            </div>
+                            <div className={styles.switch}>
+                                <input
+                                    type="checkbox"
+                                    id="history"
+                                    name="history"
+                                    checked={privacySettings.history}
+                                    onChange={handleToggleChange}
+                                />
+                                <label htmlFor="history">
+                                    <span className={styles.slider}></span>
+                                    Allow History Access
+                                </label>
+                            </div>
+                            <div className={styles.switch}>
+                                <input
+                                    type="checkbox"
+                                    id="calendar"
+                                    name="calendar"
+                                    checked={privacySettings.calendar}
+                                    onChange={handleToggleChange}
+                                />
+                                <label htmlFor="calendar">
+                                    <span className={styles.slider}></span>
+                                    Allow Calendar Access
+                                </label>
+                            </div>
+                            <div className={styles.switch}>
+                                <input
+                                    type="checkbox"
+                                    id="documents"
+                                    name="documents"
+                                    checked={privacySettings.documents}
+                                    onChange={handleToggleChange}
+                                />
+                                <label htmlFor="documents">
+                                    <span className={styles.slider}></span>
+                                    Allow Documents Access
+                                </label>
+                            </div>
+                            <button type="submit" className={styles.submitButton}>Submit Options</button>
+                        </form>
+                    </div>
+                </div>
+                
+                
             )}
+            
         </div>
-        
     );
 }
-
